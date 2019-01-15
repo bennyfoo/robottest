b161f4b6e58b86c9bbcc99f34eceba696602a7a7
benny <bennyfoo@163.com>
2019-01-16 01:01:31 +0800

    添加项目文件。

---
+++ robotclient/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace robotclient
+{
+    partial class FrmClient
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_IP = new System.Windows.Forms.TextBox();
+            this.txt_Port = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.txt_Log = new System.Windows.Forms.RichTextBox();
+            this.txt_Msg = new System.Windows.Forms.RichTextBox();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_IP
+            // 
+            this.txt_IP.Location = new System.Drawing.Point(56, 44);
+            this.txt_IP.Name = "txt_IP";
+            this.txt_IP.Size = new System.Drawing.Size(167, 28);
+            this.txt_IP.TabIndex = 0;
+            this.txt_IP.Text = "192.168.1.7";
+            this.txt_IP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_Port
+            // 
+            this.txt_Port.Location = new System.Drawing.Point(299, 42);
+            this.txt_Port.Name = "txt_Port";
+            this.txt_Port.Size = new System.Drawing.Size(84, 28);
+            this.txt_Port.TabIndex = 1;
+            this.txt_Port.Text = "6000";
+            this.txt_Port.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Connect.Location = new System.Drawing.Point(531, 36);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(120, 36);
+            this.btn_Connect.TabIndex = 2;
+            this.btn_Connect.Text = "连接";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Close.Location = new System.Drawing.Point(692, 35);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(135, 37);
+            this.btn_Close.TabIndex = 3;
+            this.btn_Close.Text = "断开连接";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_CloseConnect_Click);
+            // 
+            // txt_Log
+            // 
+            this.txt_Log.Location = new System.Drawing.Point(26, 108);
+            this.txt_Log.Name = "txt_Log";
+            this.txt_Log.Size = new System.Drawing.Size(816, 192);
+            this.txt_Log.TabIndex = 4;
+            this.txt_Log.Text = "";
+            // 
+            // txt_Msg
+            // 
+            this.txt_Msg.Location = new System.Drawing.Point(26, 324);
+            this.txt_Msg.Name = "txt_Msg";
+            this.txt_Msg.Size = new System.Drawing.Size(816, 209);
+            this.txt_Msg.TabIndex = 5;
+            this.txt_Msg.Text = "";
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Send.Location = new System.Drawing.Point(653, 579);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(151, 34);
+            this.btn_Send.TabIndex = 6;
+            this.btn_Send.Text = "发送消息";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // FrmClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 658);
+            this.Controls.Add(this.btn_Send);
+            this.Controls.Add(this.txt_Msg);
+            this.Controls.Add(this.txt_Log);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.txt_Port);
+            this.Controls.Add(this.txt_IP);
+            this.Name = "FrmClient";
+            this.Text = "测试机";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_IP;
+        private System.Windows.Forms.TextBox txt_Port;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.RichTextBox txt_Log;
+        private System.Windows.Forms.RichTextBox txt_Msg;
+        private System.Windows.Forms.Button btn_Send;
+    }
+}
+
